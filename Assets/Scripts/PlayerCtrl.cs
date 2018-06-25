5947412042df63ec1338fd1581e3b3bed4553df2
Bruno Mundim <brunomundim3232@gmail.com>
2018-06-25 10:28:01 -0300

    Controle de Animações

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+
+Idle - 0 
+Jump - 1
+Walk - 2
+Hurt - 3
+Attack - 4
+Falling - 5
+
+ */
+
 public class PlayerCtrl : MonoBehaviour {
 
 	public float horizontalSpeed = 5f;
 	public float jumpSpeed = 400f;
-
+	
 	Rigidbody2D rb;
 	SpriteRenderer sr;
+	Animator anim;
+
+	bool isJumping = false;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		sr = GetComponent<SpriteRenderer>();
-		
+		anim = GetComponent<Animator>();		
 	}
 	
 	// Update is called once per frame
@@ -43,13 +57,30 @@ public class PlayerCtrl : MonoBehaviour {
 		else if (speed > 0f){
 			sr.flipX = false;
 		}
+		
+		if(!isJumping){
+			anim.SetInteger("State", 2);
+		}		
 	}
 
 	void StopMovingHorizontal()	{
 		rb.velocity = new Vector2(0f, rb.velocity.y);
+		
+		if(!isJumping){
+			anim.SetInteger("State", 0);
+		}	
 	}
 
 	void Jump()	{
+		isJumping = true;
 		rb.AddForce(new Vector2(0f, jumpSpeed));
+		anim.SetInteger("State", 1);
 	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.layer == LayerMask.NameToLayer("Ground")){
+			isJumping = false;
+		}
+	}
+
 }

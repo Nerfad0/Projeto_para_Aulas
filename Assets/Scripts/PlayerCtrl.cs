18a495961a960d43b8984b0d5010863480d0e1be
Bruno Mundim <brunomundim3232@gmail.com>
2018-08-03 07:54:53 -0300

    Aula 03

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -30,6 +30,9 @@ public class PlayerCtrl : MonoBehaviour {
 	public bool isGrounded;
 	public LayerMask whatIsGround;
 
+	bool canDoubleJump = false;
+	public float delayForDoubleJump = 0.2f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -90,7 +93,20 @@ public class PlayerCtrl : MonoBehaviour {
 		isJumping = true;
 		rb.AddForce(new Vector2(0f, jumpSpeed));
 		anim.SetInteger("State", 1);
+
+		Invoke("EnableDoubleJump", delayForDoubleJump);
 		}
+
+		if (canDoubleJump && !isGrounded) {
+			rb.velocity = Vector2.zero;
+			rb.AddForce(new Vector2(0f, jumpSpeed));
+			anim.SetInteger("State", 1);
+			canDoubleJump = false;
+		}
+	}
+	
+	void EnableDoubleJump(){
+		canDoubleJump = true;
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
@@ -98,5 +114,4 @@ public class PlayerCtrl : MonoBehaviour {
 			isJumping = false;
 		}
 	}
-
 }
